236c2f9a3f0c4586a62881894e916bef1e7dd587
111Aaru11 <148994385+111Aaru11@users.noreply.github.com>
2024-07-23 14:26:01 +0530

    Add files via upload
    Automatically send messages on whatsapp to a specific phone number

---
+++ Sendautomatewhatsappmessage.py
@@ -0,0 +1,7 @@
+import pywhatkit as pyw #import python module
+
+#modulename.sendwhatmsg('phone number to which message needs to be sent, message, time in hrs, time in minutes)
+pyw.sendwhatmsg('+919464332961', 'Welcome to wscubetech',00,35)
+
+
+#AND IT'S DONE!!!!!!!
